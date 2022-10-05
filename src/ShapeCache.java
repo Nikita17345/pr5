c46824bf980721546c942cc0d175369ca78908ac
VARIANT 7 <Kislenko1950@gmail.com>
2022-10-05 20:32:02 +0300

    Initial commit

---
+++ src/ShapeCache.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class ShapeCache {
+    private static HashMap<String, Shape> shapeMap = new HashMap<>();
+
+    public static Shape getShape(String shapeId){
+        Shape cachedShape = shapeMap.get(shapeId);
+
+        return (Shape) cachedShape.clone();
+    }
+
+    public static void loadCache(){
+        Circle circle = new Circle();
+        circle.setId("1");
+        shapeMap.put(circle.getId(), circle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setId("2");
+        shapeMap.put(rectangle.getId(), rectangle);
+
+        Square square = new Square();
+        square.setId("3");
+        shapeMap.put(square.getId(), square);
+    }
+}
